b4101c7caa81ee1bf9dedf1d392ed040424bdfed
evsasse <evsasse@gmail.com>
2016-09-30 00:03:47 -0300

    main program uses a block

--- projeto1/main.cpp
+++ projeto1/main.cpp
@@ -7,11 +7,7 @@ extern BlockNode lines;
 
 int main(){
   yyparse();
-  for(Node *line : lines){
-    //std::cout << "Line[";
-    line->print();
-    //std::cout << "]";
-    std::cout << std::endl;
-  }
+  lines.print();
+  std::cout << std::endl;
   return 0;
 }
